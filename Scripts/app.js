d4805b5735309899521f53d63740064d6e05e3cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-20 09:58:24 -0500

    added paragraphs to the main content

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,12 +4,37 @@
 //AKA - Anonymous Self-Executing Function
 //Closure - limits scope leak
 
+"use strict";
+
 (function()
 {
 
     function Start()
     {
         console.log("App Started...");
+
+        let paragraphOneText = "This is a simple site to demonstrate DOM Manipulation for ICE 1";
+    
+        let paragraphOneElement = document.getElementById("paragraphOne");
+
+        paragraphOneElement.textContent = paragraphOneText;
+        paragraphOneElement.className = "fs-5";
+
+        // Step 1. document.createElement
+        let newParagraph = document.createElement("p");
+        // Step 2. configure the element
+        newParagraph.setAttribute("id","paragraphTwo");
+        newParagraph.textContent = "...And this is paragraph two";
+        // Step 3. select the parent element
+        let mainContent = document.getElementsByTagName("main")[0];
+        // Step 4. Add / Insert the element
+        mainContent.appendChild(newParagraph);
+
+        newParagraph.className = "fs-6";
+
+        // another way of injecting content
+        //let paragraphThree =`<p class="fs-7 fw-bold">And this is the Third Paragraph</p>`
+        //mainContent.innerHTML = paragraphThree;
     }
 
     window.addEventListener("load", Start);
