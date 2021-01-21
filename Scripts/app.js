c0db46bde9eb0b36db2b0d50c59c661e3cfabab1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-21 09:03:09 -0500

    added about, projects, services and contact pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,13 +8,11 @@
 
 (function()
 {
-
-    function Start()
+    function displayHome()
     {
-        console.log("App Started...");
+        let paragraphOneText =
+          "This is a simple site to demonstrate DOM Manipulation for ICE 1";
 
-        let paragraphOneText = "This is a simple site to demonstrate DOM Manipulation for ICE 1";
-    
         let paragraphOneElement = document.getElementById("paragraphOne");
 
         paragraphOneElement.textContent = paragraphOneText;
@@ -23,7 +21,7 @@
         // Step 1. document.createElement
         let newParagraph = document.createElement("p");
         // Step 2. configure the element
-        newParagraph.setAttribute("id","paragraphTwo");
+        newParagraph.setAttribute("id", "paragraphTwo");
         newParagraph.textContent = "...And this is paragraph two";
         // Step 3. select the parent element
         let mainContent = document.getElementsByTagName("main")[0];
@@ -33,8 +31,8 @@
         newParagraph.className = "fs-6";
 
         // another way of injecting content
-        let paragraphDiv = document.createElement('div');
-        let paragraphThree =`<p id="paragraphThree" class="fs-7 fw-bold">And this is the Third Paragraph</p>`
+        let paragraphDiv = document.createElement("div");
+        let paragraphThree = `<p id="paragraphThree" class="fs-7 fw-bold">And this is the Third Paragraph</p>`;
         paragraphDiv.innerHTML = paragraphThree;
 
         // insertions
@@ -44,22 +42,66 @@
 
         // example of inserting after a node
         newParagraph.after(paragraphDiv);
-        
+
         // deletions
-        
+
         // example of removing a single element
-        //paragraphOneElement.remove(); 
+        //paragraphOneElement.remove();
 
         // example of removeChild
         mainContent.removeChild(paragraphOneElement);
 
         // update / modification
         //mainContent.firstElementChild.textContent = "Welcome Home!";
-        
-        mainContent.innerHTML = 
-        `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
+
+        mainContent.innerHTML = `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
          <p id="paragraphOne" class="fs-3 fw-bold">This is my first Paragraph</p>
         `;
+        
+    }
+
+    function displayAbout()
+    {
+
+    }
+
+    function displayProjects()
+    {
+
+    }
+
+    function displayServices()
+    {
+
+    }
+
+    function displayContact()
+    {
+
+    }
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        switch (document.title) 
+        {
+          case "Home":
+              displayHome();
+            break;
+          case "About":
+              displayAbout();
+            break;
+          case "Projects":
+              displayProjects();
+            break;
+          case "Services":
+              displayServices();
+            break;
+          case "Contact":
+              displayContact();
+            break;
+        }
         
     }
 
